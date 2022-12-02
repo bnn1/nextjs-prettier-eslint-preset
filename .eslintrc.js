bb231e00a32a070c319507b364abe08aeee9c6ef
Boris Novikov <boris.novikov95@gmail.com>
2022-12-02 19:37:37 +0400

    feat: eslint/prettier settings

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort', 'import'],
+  rules: {
+    /* IMPORT SORT */
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+          // Note that if you use the `node:` prefix for Node.js builtins,
+          // you can avoid this complexity: You can simply use "^node:".
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          // Packages. `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    /* CODE STYLE */
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['case', 'default'], next: '*' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Next.js Eslint preset
+
+1. Create new Next.js app `yarn create next-app my-app`
+2. `cd` into app folder and install eslint dependencies `yarn add -D eslint-config-prettier eslint-plugin-import eslint-plugin-prettier eslint-plugin-simple-import-sort prettier`
+3. Copy `eslintrc.js` and `.prettierrc` from this repo into your Next.js app root
